018edec7fcb14151b81a15b667cb1ef8397d992c
Sthembiso Musana <sthembisomusana2@gmail.com>
2021-10-20 03:53:49 +0200

    Updated the chat protocol

--- Server-Side/App.js
+++ Server-Side/App.js
@@ -11,8 +11,8 @@ class User{
         this.groupMessageList = [];
     }
 
-    appendMessageHistory(message){
-        
+    appendFriendList(user){
+        this.friendsList.push(user);
     }
     updateMessageList(message){
         this.recentMessages.push(message);
@@ -22,6 +22,19 @@ class User{
     isRecentEmpty(){
         return (this.recentMessages.length === 0);
     }
+
+    friendListToJSON(){
+        let tempObject = {length:this.friendsList.length};
+        if(tempObject.length > 0){
+            for(let i=0; i<tempObject.length; i++){
+                tempObject['friend '+i] = this.friendsList[i].toJSON();
+            }
+            return(tempObject);
+        }
+        return null;
+        
+    }
+
     recentListJSON(){
         if(!this.isRecentEmpty()){
             let tempObj = {};
@@ -30,18 +43,27 @@ class User{
             }
             return(tempObj);
         }
-        return null;
+        return {};
         
     }
-}
 
+    toJSON(){
+        return({
+            username:this.username,
+            email:this.email,
+            id:this.id,
+            messages:this.recentListJSON(),
+            friends:this.friendListToJSON()
+        });
+    }
+}
 
 class Group{
     constructor(name, participants){
         this.name = name;
         this.participants = participants; // list of participants
         this.id = null;
-        this.messageHistory = null;
+        this.messageHistory = [];
     }
 
     setMessageHistory(historyList){
@@ -61,6 +83,23 @@ const { endianness } = require('os');
 let app = express();
 const PORT = 8090 || process.env.PORT;
 const users = []
+const testUser = new User('Sthembiso Musana', '12341241', 'sthembisomusana2@gmail.com');
+
+const TestUsers =  []
+for(let i = 0; i<10; i++){
+    let tempUser = new User("Sthembiso"+i, "1234123123+"+i+"1212"+i+i, "sthembiso@"+i);
+    TestUsers.push(tempUser);
+
+}
+
+testUser.friendsList = TestUsers;
+
+
+TestUsers.sort((user1, user2)=>{
+    if(user1.username > user2.username)return 1;
+    else if(user1.username < user2) return -1;
+    return 0;
+});
 
 
 app.listen(PORT, function(){
@@ -74,8 +113,13 @@ app.post('/send', (req, res)=>{
     let messageRef = JSON.parse(req.body);
     console.log(messageRef)
     if(messageRef.recipientType === 'single'){
-        
+
     }
+});
+
+app.get('/users', (req, res)=>{
+    console.log(JSON.stringify(testUser.toJSON()));    
+    res.end(JSON.stringify(testUser.toJSON()));    
 });
 
 app.post('/signin', (req, res)=>{
@@ -86,25 +130,11 @@ app.post('/signin', (req, res)=>{
         let tempUser = new User(resBody.username, resBody.userId, resBody.userEmail);
         users.push(tempUser);
     }
-    console.log(users);
     
-})
+});
 
 app.post('/poll', (req, res)=>{
     console.log(JSON.parse(req.body));
-
-})
-
-const TestUsers =  []
-for(let i = 0; i<1000000; i++){
-    let tempUser = new User("Sthembiso"+i, "1234123123+"+i+"1212"+i+i, "sthembiso@"+i);
-    TestUsers.push(tempUser);
-}
-
-TestUsers.sort((user1, user2)=>{
-    if(user1.username > user2.username)return 1;
-    else if(user1.username < user2) return -1;
-    return 0;
 });
 
 
